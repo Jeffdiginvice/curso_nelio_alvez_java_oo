8db589da81b80a0c4c6cc211be85569a5af6870c
Jefferson Castro <jeffersondiginvice7@gmail.com>
2023-10-05 14:25:46 -0300

    estrutura repetitiva while

--- src/estrutura_repetitiva/Aula_while.java
+++ src/estrutura_repetitiva/Aula_while.java
@@ -1,9 +1,24 @@
 package estrutura_repetitiva;
 
+import java.util.Scanner;
+
 public class Aula_while {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		int soma = 0;
+		while(x != 0) {
+			soma = soma + x;
+			x = sc.nextInt();
+		}
+		
+		System.out.println(soma);
+		
+		sc.close();
 
 	}
 
