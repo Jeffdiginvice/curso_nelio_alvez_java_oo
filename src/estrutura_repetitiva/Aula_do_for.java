53961da04d697b249c788ec57245c3daa15c9ad1
Jefferson Castro <jeffersondiginvice7@gmail.com>
2023-10-10 15:04:59 -0300

    do-while

---
+++ src/estrutura_repetitiva/Aula_do_for.java
@@ -0,0 +1,30 @@
+package estrutura_repetitiva;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula_do_for {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		char resp;
+		do {
+			System.out.println("Digite a temperatura em celsius");
+			double C = sc.nextDouble();
+			double F = 9.0 * C / 5.0 + 32.0;
+			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+			System.err.println("Deseja repetir! (s/n)");
+			resp = sc.next().charAt(0);
+					
+		}while(resp != 'n');
+		
+		System.out.println("Porgrama encerrado!");
+		sc.close();
+
+	}
+
+}
--- src/estrutura_repetitiva/Aula_for.java
+++ src/estrutura_repetitiva/Aula_for.java
@@ -1,5 +1,25 @@
 package estrutura_repetitiva;
 
-public class Aula_for {
+import java.util.Scanner;
 
+public class Aula_for {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int soma = 0;
+		for(int i = 0; i < n; i++){
+			
+			int x = sc.nextInt();
+			soma = soma + x;
+		}
+		
+		System.out.println(soma);
+		
+		sc.close();	
+		
+	}
 }
